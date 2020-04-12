1715ed503211df94b3a3689ec5edd582ef080ea7
laviedegeorge <laviedegeorge1292@gmail.com>
2020-04-12 21:28:31 +0100

    challange 1 done!

--- src/estimator.js
+++ src/estimator.js
@@ -5,9 +5,9 @@ const estimateCurrentlyInfected = (impact, data) => {
   return currentlyInfected;
 };
 const infectionByRequestTime = (data, currentlyInfected) => {
-  /* if (data.periodType.toLowerCase() === 'days') {
+  if (data.periodType.toLowerCase() === 'days') {
     data.timeToElapse *= 1;
-  } */
+  }
   if (data.periodType.toLowerCase() === 'weeks') {
     data.timeToElapse *= 7;
   }
@@ -15,29 +15,13 @@ const infectionByRequestTime = (data, currentlyInfected) => {
     data.timeToElapse *= 30;
   }
   const time = Math.trunc(data.timeToElapse / 3);
-  // eslint-disable-next-line no-restricted-properties
-  const infectionBRT = currentlyInfected * (Math.trunc(Math.pow(2, time)));
+
+  const infectionBRT = currentlyInfected * (Math.trunc(2 ** time));
   return infectionBRT;
 };
 
 // Changenge 2 functions
-// eslint-disable-next-line arrow-body-style
-const estSevereCasesBRT = (currentlyInfected) => {
-  return currentlyInfected * 512 * 0.15;
-};
-// eslint-disable-next-line arrow-body-style
-const estHospitalBRT = (data, severeCasesByRequestTime) => {
-  return data.totalHospitalBeds * 0.35 - severeCasesByRequestTime;
-};
 
-// Changenge 3 functions
-const estimateCasesForICUByRequestedTime = (infectionRT) => infectionRT * 0.05;
-const casesVentilatorsBRT = (infectionRT) => infectionRT * 0.02;
-const estimateDollarsInFlight = (data, infectionRT) => {
-  const dollarsInFlight = (infectionRT * 0.65)
-  * data.region.avgDailyIncomeInUSD * 30;
-  return dollarsInFlight;
-};
 
 // Estimate Impact function
 const estimateImpact = (data, impact) => {
@@ -46,23 +30,12 @@ const estimateImpact = (data, impact) => {
   const infectionByRT = Math.trunc(infectionByRequestTime(data, currentlyInfectedR));
 
   // Challenge 2
-  const severeCasesByRequestedTime = estSevereCasesBRT(currentlyInfectedR);
-  const hospitalBedsByRequestedTime = estHospitalBRT(data, severeCasesByRequestedTime);
 
-  // Challenge 3
-  const casesForICUByRequestedTime = estimateCasesForICUByRequestedTime(infectionByRT);
-  const casesForVentilatorsByRequestedTime = casesVentilatorsBRT(infectionByRT);
-  const dollarsInFlightForImpact = estimateDollarsInFlight(data, infectionByRT);
 
   // Impact object
   const impactObj = {
     currentlyInfected: currentlyInfectedR,
-    infectionByRequestedTime: infectionByRT,
-    severeCasesByRequestedTime: Math.trunc(severeCasesByRequestedTime),
-    hospitalBedsByRequestedTime: Math.trunc(hospitalBedsByRequestedTime),
-    casesForICUByRequestedTime: Math.trunc(casesForICUByRequestedTime),
-    casesForVentilatorsByRequestedTime: Math.trunc(casesForVentilatorsByRequestedTime),
-    dollarsInFlight: dollarsInFlightForImpact
+    infectionByRequestedTime: infectionByRT
   };
   return impactObj;
 };
